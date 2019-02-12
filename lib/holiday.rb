bd81c5439133f1ff7801c1c0148e4bf752b3e7a4
Tao Xu <nytaoxu@gmail.com>
2019-02-12 00:09:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{key.capitalize}:"
     value.each do |x, y|
       # binding.pry
-      puts "  #{x}: #{y.join(", ")}"
+      puts "  #{x}: #{y.join(', ')}"
     end
   end
 end
