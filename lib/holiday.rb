0bf4583fb21c1258421fc9611492be65ad0d5448
Tao Xu <nytaoxu@gmail.com>
2019-02-12 00:06:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    puts "#{key}:"
+    puts "#{key.capitalize}:"
     value.each do |x, y|
       puts "  #{x}: #{value.join(", ")}"
     end
